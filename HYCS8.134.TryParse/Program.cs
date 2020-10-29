d07ca01c3e27893acca06d064c488bf2eb93df14
Adem AKKUŞ <32845253+ademakkus@users.noreply.github.com>
2020-10-29 15:04:51 +0200

    Add files via upload

---
+++ HYCS8.134.TryParse/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using  static System.Console;
+namespace HYCS8._134.TryParse
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string value = "29/10/2020";
+			DateTime dateTime;
+			if (DateTime.TryParse(value,out dateTime))
+			///	if (DateTime.TryParse(value, out _)) //discard _ with c#7 
+				{
+				WriteLine(dateTime.ToLongDateString()); 
+				WriteLine(dateTime.ToShortDateString());
+				WriteLine(DateTime.TryParse(value, out dateTime));
+			}
+
+			ReadKey();
+		}
+	}
+}
