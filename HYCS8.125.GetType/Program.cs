b3fcbb0c5e68b876aca5b666f5f6c7d90c26d4d6
Adem AKKUŞ <32845253+ademakkus@users.noreply.github.com>
2020-10-29 14:55:51 +0200

    Add files via upload

---
+++ HYCS8.125.GetType/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using  static System.Console;
+namespace HYCS8._125.GetType
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var name = "Adem AKKUŞ";
+			WriteLine(name.GetType().ToString());
+			var pi = 3.14;
+			WriteLine(pi.GetType().ToString());
+			var decision = true;
+			WriteLine(decision.GetType().ToString());
+			ReadKey();
+		}
+	}
+}
---
+++ HYCS8.126.TypeOf/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+
+namespace HYCS8._126.TypeOf
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			var name = "Adem AKKUŞ";
+			if (name.GetType()==typeof(string))
+			{
+				WriteLine("Type of name is string");
+			}
+		}
+	}
+}
