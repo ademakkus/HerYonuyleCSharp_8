2f5498d68a879e82462f63027d7ef52cf6daaf63
Adem AKKUŞ <32845253+ademakkus@users.noreply.github.com>
2020-10-29 14:54:26 +0200

    Add files via upload

---
+++ HYCS8.120.Ref/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HYCS8._120.Ref
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string message = "This message is created by main program";
+			Console.WriteLine(message);
+			WriteMessageMethod(ref message);
+			Console.ForegroundColor = ConsoleColor.DarkBlue;
+			Console.WriteLine(message);
+			Console.ReadKey();
+		}
+		
+		private static void WriteMessageMethod(ref string message)
+		{
+			message = "This message is created by WriteMessageMethod method";
+		}
+	}
+}
